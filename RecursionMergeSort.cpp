808b8c757d887b2b35f30ef2f2dd8b6007cce083
Raj Khatri <khatriraj030@gmail.com>
2022-07-08 20:54:55 +0530

    MergeSort Recursion

---
+++ RecursionMergeSort.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+void merge(int *arr, int start, int end) {
+    int mid = start + (end - start) / 2;
+    int len1 = mid - start + 1;
+    int len2 = end - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    int mainArrayIndex = start;
+    for (int i = 0; i < len1; i++) {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid + 1;
+    for (int i = 0; i < len2; i++) {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    int index1 = 0, index2 = 0;
+    mainArrayIndex = start;
+
+    while (index1 < len1 && index2 < len2) {
+        if (first[index1] < second[index2]) {
+            arr[mainArrayIndex++] = first[index1++];
+        } else {
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }
+
+    while (index1 < len1) {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while (index2 < len2) {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    delete[] first;
+    delete[] second;
+}
+
+void mergeSort(int *arr, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    int mid = start + (end - start) / 2;
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, start, end);
+}
+
+int main() {
+    int arr[10] = {5, 6, 4, 1, 0, 9, 2, 7, 8, 3};
+    int size = 10;
+    mergeSort(arr, 0,----- size - 1);
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    return 0;
+}
